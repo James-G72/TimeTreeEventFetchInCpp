22a33bd2cbabb5fdbe820c62eb63992bfcf7faa9
James Gower <66065325+James-G72@users.noreply.github.com>
2026-02-08 09:34:01 +0000

    Give example config file

--- include/tt_config.h
+++
@@ -1,3 +0,0 @@
-
-char const * TT_USERNAME = "jmgower86@gmail.com";
-char const* TT_PASSWORD = "ntXUanjuKRY2BQz";
---
+++ include/tt_config_example.h
@@ -0,0 +1,3 @@
+
+char const * TT_USERNAME = "youremail@youremailprovider.com";
+char const* TT_PASSWORD = "y0urp@55w0rd";
